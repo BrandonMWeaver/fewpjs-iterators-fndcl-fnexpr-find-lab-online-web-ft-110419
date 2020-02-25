8df6fc12cfbb066c459a33d8b5bfd21ca3409bd8
Brandon Weaver <a7xncob@gmail.com>
2020-02-25 21:23:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function testFunc() {
 }
 
 function superbowlWin(record) {
-  return !!record.find(function(record) {
+  return record.find(function(record) {
     return record.result === 'W';
-  }) ? year : undefined;
+  });
 }
