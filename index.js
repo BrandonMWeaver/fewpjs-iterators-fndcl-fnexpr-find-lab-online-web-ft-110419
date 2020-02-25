103267b9c7cc33b49bf740c84f435e651fab84a4
Brandon Weaver <a7xncob@gmail.com>
2020-02-25 21:30:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const testVar = {};
-
-function testFunc() {
-  return "hi";
-}
-
 function superbowlWin(records) {
   let result = records.find(function(record) {
     return record.result === 'W';
